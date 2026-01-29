03db0efb2400ba7098071a67299d851ceb1d243e
Sashwot Khadgi <100107745+0rdinary-guy@users.noreply.github.com>
2026-01-29 08:33:15 +0545

    Delete rest_pro directory

--- rest_pro/rest_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Menu
-
-class MenuForm(forms.ModelForm):
-    class Meta:
-        model = Menu
-        fields = ['Name', 'Description', 'Price', 'Image']
--- rest_pro/rest_app/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_pro import urls
-from django.urls import path
-from . import views
-from .views import home,menu_list
-
-urlpatterns = [
-    path("", home, name='home'),
-    path('list/', views.menu_list, name='menu_list'),
-]
